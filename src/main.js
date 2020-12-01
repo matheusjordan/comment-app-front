88543b1401dccf107dae3e6c9577ea166bfbf526
= <=>
2020-12-01 20:49:24 -0300

    Configurado toast

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import Toaster from '@meforma/vue-toaster';
 const app = createApp(App);
 
 app.use(Toaster, {
-    position: 'top'
+    position: 'top-right',
+    duration: 2000
 });
 app.mount('#app');
